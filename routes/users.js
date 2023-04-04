fb45756fef2c87b65d2965e1137eacd54f83fd1b
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2023-04-04 12:42:47 -0700

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/login', function(req,res,next){
+  res.status(200).json({
+    message: "Wild Success!!"
+  })
+})
+module.exports = router;
